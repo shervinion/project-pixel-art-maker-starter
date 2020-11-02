28a5d9e6c6cb009182b1b0ce1ee40793eac15406
shervinion <shervinion@users.noreply.github.com>
2020-11-02 18:43:43 -0500

    Update designs.js
    Project Pixel Art Maker done. Ready for review.

--- designs.js
+++ designs.js
@@ -2,9 +2,60 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+// console.log('Hello World! - by shervin');
+
+const form = document.querySelector("#sizePicker");
+const canvasTable = document.querySelector('#pixelCanvas');
+const cellColor = document.querySelector('#colorPicker');
+
 
 function makeGrid() {
+    // Your code goes here!
+    //console.log('Shervin, start of code');
 
-// Your code goes here!
+    // Clear table
+    canvasTable.innerHTML = "";
 
+    // Get Height and Width
+    let dimensions = getGridDimension();
+    let inputHeight = dimensions.inputHeight;
+    let inputWidth = dimensions.inputWidth;
+
+    // Test adding a paragraph
+    //let para1 = document.createElement('p');
+    //para1.textContent = "right now";
+    //canvasTable.appendChild(para1);
+
+    // Build table
+    for (r = 1; r <= inputHeight; r++){
+        let row = document.createElement('tr');
+        //row.textContent = "i" + r;
+        canvasTable.appendChild(row);
+        for (c = 1; c<= inputWidth; c++){
+            let cell = document.createElement('td');
+            //cell.textContent = r + ',' + c;
+            row.appendChild(cell);
+            cell.addEventListener('click', function(event){
+            event.target.style.backgroundColor = cellColor.value;
+            // TODO - How to drag mouse and fill colors instead of clicking on each cell individually?
+            })
+        }
+        }
 }
+
+function getGridDimension() {
+    // Probably it's an overkill to have a function to return querySelector's value
+    // but it's fun to refactor, and also to use arrays!
+    const inputHeight = document.querySelector('#inputHeight').value;
+    const inputWidth = document.querySelector('#inputWidth').value;
+    console.log(inputHeight);
+    console.log(inputWidth);
+    return {inputWidth, inputHeight};
+}
+
+
+form.addEventListener('submit', function(event) {
+    // I'm not sure why submit button refreshes the page so had to use prevenetDefault.
+    event.preventDefault();
+    makeGrid();
+    })
